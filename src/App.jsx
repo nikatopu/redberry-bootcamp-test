cbddb68413ba04d961c4b7125b769926dba0974d
nikatopu <nikatopu@gmail.com>
2024-09-13 13:47:20 +0400

    Added Card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Header from "./UI elements/Header/header";
+import Card from "./UI elements/Card/card";
 
 function App() {
     return <div className="App">
         <Header></Header>
+        <Card></Card>
     </div>
 }
 

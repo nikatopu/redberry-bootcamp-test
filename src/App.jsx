a97d0156d6c83a73dd33fa5d6e549a3ba5ce1563
nikatopu <nikatopu@gmail.com>
2024-09-13 12:50:40 +0400

    Created Header component
    Created the simple header component

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Header from "./UI elements/Header/header";
+
+function App() {
+    return <div className="App">
+        <Header></Header>
+    </div>
+}
+
+export default App;
---
+++ src/UI elements/Header/header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./header.css"
+
+function Header() {
+    return <header>
+        <img src={"./redberry-logo.png"} alt="redberry-logo"/>
+    </header>;
+}
+
+export default Header;
